d837ebc8234490996e0fa127562db59a0d8d87ad
GOKUL GG <gokulraja143@gmail.com>
2024-11-07 00:01:35 +0530

    Some Animation added to the roles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,51 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/About.css";
+import { useState,useEffect } from "react";
 
-const About = () => {
+  const About = () => {
+
+  const roles = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Hardware Engineer",
+    "Artist"
+  ];
+
+  const [currentRole, setCurrentRole] = useState(roles[0]);
+
+  useEffect(() => {
+    const roleInterval = setInterval(() => {
+      setCurrentRole((prevRole) => {
+        const currentIndex = roles.indexOf(prevRole);
+        const nextIndex = (currentIndex + 1) % roles.length; 
+        return roles[nextIndex];
+      });
+    }, 2000); 
+
+    return () => clearInterval(roleInterval);
+  }, []);
+
   return (
     <section id="about" className="pt-5 about-section">
       <Container>
         <Row className="align-items-center">
           <Col md={12} className="text-center mb-4">
             <h1>I'm Gokul G G</h1>
-            <p>MERN Stack Developer</p>
+            <p>{currentRole}</p>
           </Col>
           <Col md={12} className="text-center">
+            <p className="name">Gokul GG</p>
             <img
               src={`../images/small.png`}
               alt=""
               className="img-fluid"
-              style={{ maxWidth: "100%", height: "auto" }}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                position: "relative",
+                zIndex: 2,
+              }}
             />
           </Col>
           <p className="new">Gokul G G</p>
